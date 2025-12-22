c6dc6dabad2ba525189bbbd1271a0d9026b00152
garinakkccsh87314 <garina_kkccsh_87314@outlook.com>
2025-12-22 14:05:03 -0800

    modern-gchat3

--- examples/nextjs-chat/src/lib/adapters.ts
+++ examples/nextjs-chat/src/lib/adapters.ts
@@ -49,6 +49,8 @@ function createVercelOIDCAuth() {
   const client = new IdentityPoolClient({
     audience,
     subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
+    type: "external_account",
+    token_url: "https://sts.googleapis.com/v1/token",
     service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${serviceAccountEmail}:generateAccessToken`,
     subject_token_supplier: new VercelOidcSupplier(),
   });
